d863adf044bf22346a0ead3e1d37a440f2ca6a43
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-01-30 22:37:04 +0000

    Arduino code is working

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#define UpPin 3
+#define DownPin 4
+#define LeftPin 5
+#define RightPin 2
 
+byte State = 0;
+
+void setup() {
+  pinMode(UpPin, INPUT_PULLUP);
+  pinMode(DownPin, INPUT_PULLUP);
+  pinMode(LeftPin, INPUT_PULLUP);
+  pinMode(RightPin, INPUT_PULLUP);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(Serial.available() > 0){
+    Serial.read();
+    State = 0;
+    State += digitalRead(UpPin) * 8;
+    State += digitalRead(DownPin) * 4;
+    State += digitalRead(LeftPin) * 2;
+    State += digitalRead(RightPin) * 1;
+    Serial.println(State);
+  }
 
 }
